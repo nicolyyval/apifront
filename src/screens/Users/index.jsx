5cfe2059806388a811ad3cabef79754b7a537457
unknown <nicoly.ferreira@aluno.senai.br>
2024-04-22 16:34:06 -0300

    installing axios

--- src/screens/Users/index.jsx
+++ src/screens/Users/index.jsx
@@ -1,9 +1,10 @@
 import { View } from "react-native";
-
+import axios from "axios";
 import styles from "./styles";
 import Title from "../../components/Title";
 
 export default function Users() {
+  
   return (
     <View style={styles.container}>
       <Title title="Usuários" />
