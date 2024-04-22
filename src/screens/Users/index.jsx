9ef7d41c1a8a4a71a2f3483456cc38792d0062fb
unknown <nicoly.ferreira@aluno.senai.br>
2024-04-22 16:42:08 -0300

    criando usuarios

--- src/screens/Users/index.jsx
+++ src/screens/Users/index.jsx
@@ -2,9 +2,19 @@ import { View } from "react-native";
 import axios from "axios";
 import styles from "./styles";
 import Title from "../../components/Title";
+import { useState } from "react";
 
 export default function Users() {
-  
+  const [users, setUsers] = useState([]);
+  const apiURL = process.env.EXPO_PUBLIC_API_URL;
+  const getUsers = async () => {
+    try {
+      const response = await axios.get(`${apiURL}/users`);
+      setUsers(response.data.users);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <View style={styles.container}>
       <Title title="Usuários" />
